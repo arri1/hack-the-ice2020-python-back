03813844fafca160901fe8c1282d2a2d72b6f860
Alex M <frohenk@gmail.com>
2020-12-26 16:31:53 +0900

    Add category filtering to companies

--- dollar/api/endpoints/companies.py
+++ dollar/api/endpoints/companies.py
@@ -5,7 +5,7 @@ from flask_restx import Resource
 from dollar.api.models.requests import pagination_arguments
 from dollar.api.models.responses import page_of_companies
 from dollar.api.restx import api
-from dollar.db.companies_table import get_companies_count_and_page
+from dollar.db.companies_table import get_companies_count_and_page, get_companies_count_and_page_by_category
 
 log = logging.getLogger(__name__)
 ns = api.namespace('companies', description='Companies endpoints')
@@ -26,6 +26,32 @@ class CompaniesCollection(Resource):
         sort_by = args.get('sort_by', 'rate')
         is_descending = args.get('is_descending', 1)
         total, items = get_companies_count_and_page(page, per_page, sort_by, is_descending)
+        result = {
+            'page': page,
+            'per_page': per_page,
+            'total': total,
+            'is_descending': is_descending,
+            'sort_by': sort_by,
+            'items': items
+        }
+        return result
+
+
+@ns.route('/category/<int:category_id>')
+class CompaniesByCategoryCollection(Resource):
+
+    @api.expect(pagination_arguments)
+    @api.marshal_with(page_of_companies)
+    def get(self, category_id: int):
+        """
+        Returns list of companies.
+        """
+        args = pagination_arguments.parse_args(request)
+        page = args.get('page', 0)
+        per_page = args.get('per_page')
+        sort_by = args.get('sort_by', 'rate')
+        is_descending = args.get('is_descending', 1)
+        total, items = get_companies_count_and_page_by_category(page, per_page, sort_by, is_descending, category_id)
         result = {
             'page': page,
             'per_page': per_page,
--- dollar/db/companies_table.py
+++ dollar/db/companies_table.py
@@ -12,6 +12,15 @@ def get_companies_count_and_page(page, per_page, sort_by, is_descending):
     return total, items
 
 
+def get_companies_count_and_page_by_category(page, per_page, sort_by, is_descending, category):
+    total = companies_db.count_documents({'category': category})
+    items = list(
+        companies_db.find({'category': category}).sort(sort_by, direction=(
+            pymongo.DESCENDING if is_descending else pymongo.ASCENDING)).skip(
+            page * per_page).limit(per_page))
+    return total, items
+
+
 def update_companies_collection(companies):
     companies_db.drop()
     companies_db.insert_many(companies)
